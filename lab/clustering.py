347f7d5782590fc865b9b95c4685c058a94027e0
Sadpotato <alexprgrigoriev@gmail.com>
2019-10-29 11:10:49 +0400

    dendrogram visualisation fixed

--- lab/clustering.py
+++ lab/clustering.py
@@ -62,49 +62,55 @@ sector_color = {'Utilities': '#FF7400', #orange
                 'Technology':'#B4ACE3' #dark blue-pink
                 }
 
-X = csr_matrix(d)
-Tcsr = minimum_spanning_tree(X).toarray()
-plt.figure(figsize=(10, 10))
-T = nx.Graph()
-T.add_nodes_from(range(1, len(d) + 1))
-for i in range(len(Tcsr)):
-    for j in range(len(Tcsr[i])):
-        if Tcsr[i][j] != 0:
-            T.add_edge(i + 1, j + 1, weight=Tcsr[i][j])
-
-
-nc = [sector_color.get(label_info[index_label[i]][0], '#24AF24')
-      for i in range(1, len(T.nodes()) + 1)]
-nx.draw_networkx(T, node_color = nc, labels=index_label, width=0.4,
-                 font_family='sans-serif', font_weight='light',
-                 font_size=7, node_size=100)
-plt.show()
-
-
-### convert the redundant n*n square matrix form into a condensed nC2 array
-### distArray[{n choose 2}-{n-i choose 2} + (j-i-1)] is the distance between
-###   points i and j
-##distArray = ssd.squareform(d) 
+##X = csr_matrix(d)
+##Tcsr = minimum_spanning_tree(X).toarray()
+##plt.figure(figsize=(10, 10))
+##T = nx.Graph()
+##T.add_nodes_from(range(1, len(d) + 1))
+##for i in range(len(Tcsr)):
+##    for j in range(len(Tcsr[i])):
+##        if Tcsr[i][j] != 0:
+##            T.add_edge(i + 1, j + 1, weight=Tcsr[i][j])
 ##
-###single : minimum, complete : maximum, average, centroid
-##distance_definition = 'centroid'
-##linked = linkage(distArray, distance_definition)
-##labelList = [index_label[i] for i in range(1, len(d) + 1)]
 ##
-##plt.figure(figsize=(9, 8))  
-##dendrogram(linked,  
-##            orientation='top',
-##            labels=labelList,
-##            distance_sort='ascending',
-##            show_leaf_counts=True,
-##           above_threshold_color='#222222',
-##           #color_threshold=1.05
-##           )
-##plt.ylabel('distance')
-##plt.title('Hierarchical Clustering Dendrogram')
-##ax = plt.gca()
-##ax.set_facecolor((0.85, 0.85, 0.85))
+##nc = [sector_color.get(label_info[index_label[i]][0], '#24AF24')
+##      for i in range(1, len(T.nodes()) + 1)]
+##nx.draw_networkx(T, node_color = nc, labels=index_label, width=0.4,
+##                 font_family='sans-serif', font_weight='light',
+##                 font_size=7, node_size=100)
 ##plt.show()
+
+
+# convert the redundant n*n square matrix form into a condensed nC2 array
+# distArray[{n choose 2}-{n-i choose 2} + (j-i-1)] is the distance between
+#   points i and j
+distArray = ssd.squareform(d) 
+
+#single : minimum, complete : maximum, average, centroid
+distance_definition = 'complete'
+linked = linkage(distArray, distance_definition)
+labelList = [index_label[i] for i in range(1, len(d) + 1)]
+
+def color_fun(link):
+    print(link)
+    return sector_color['Energy']
+
+plt.figure(figsize=(9, 8))  
+dendrogram(linked,  
+            orientation='top',
+           no_labels=True,
+            #labels=labelList,
+            distance_sort='ascending',
+            show_leaf_counts=True,
+            color_threshold=1.3,
+           #link_color_func=lambda k: color_fun(k),
+           above_threshold_color='#222222'
+           )
+plt.ylabel('distance')
+#plt.title('Hierarchical Clustering Dendrogram')
+ax = plt.gca()
+ax.set_facecolor((0.85, 0.85, 0.85))
+plt.show()
 
 
 
